7e3fcbe153037ebb6fbbb105d800408ea0620191
Felipe Romero <feliperomero3@outlook.com>
2021-06-29 23:35:14 -0500

    Display the books on the home page.

--- BookStore/Pages/Index.cshtml.cs
+++ BookStore/Pages/Index.cshtml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using BookStore.Data;
+using BookStore.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace BookStore.Pages
@@ -11,15 +11,19 @@ namespace BookStore.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext context, ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Book> Books { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Books = await _context.Books.AsNoTracking().ToListAsync();
         }
     }
 }
